446a331fcee13c1a971c344b960f713dd7c8bccd
HamidZahid <hamidchaudhary3867@gmail.com>
2025-02-05 22:37:42 +0500

    Array Tuple Enumeration Added

--- Hamid.js
+++ Hamid.js
@@ -1,2 +1,12 @@
 "use strict";
 var a = 12;
+//Array
+let arr = [1, 2, 3, 4, 5];
+//Tuples
+let arr2 = [1, "Hamid", true];
+//Enumerations
+var Color;
+(function (Color) {
+    Color["NAME"] = "hamid";
+    Color[Color["AGE"] = 12] = "AGE";
+})(Color || (Color = {}));
--- Hamid.ts
+++ Hamid.ts
@@ -1,1 +1,13 @@
 var a = 12;
+
+//Array
+
+let arr = [1, 2, 3, 4, 5];
+
+//Tuples
+
+let arr2: [number, string, boolean] = [1, "Hamid", true];
+
+//Enumerations
+
+enum Color { NAME = 'hamid', AGE = 12 }
