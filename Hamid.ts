1efa9b249bb45029138cdc30a66b41cfd80b08fb
HamidZahid <hamidchaudhary3867@gmail.com>
2025-02-17 22:37:53 +0500

    Classes and Object

--- Hamid.ts
+++ Hamid.ts
@@ -1,5 +1,13 @@
-//Type Alises
-
-type StringOrNumber = string | number;
-
-let itemID: StringOrNumber;
+//Classes and object
+class person{
+    name: string;
+    age: number;
+    constructor(name: string, age: number){
+        this.name = name;
+        this.age = age;
+    }
+    display(): void{
+        console.log(this.name);
+        console.log(this.age);
+    }
+}
