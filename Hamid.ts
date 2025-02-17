970e4d2448c82ed78f5537b32f2c6e5658aa3158
HamidZahid <hamidchaudhary3867@gmail.com>
2025-02-17 22:06:53 +0500

    type alises

--- Hamid.ts
+++ Hamid.ts
@@ -1,48 +1,5 @@
-var a = 12;
+//Type Alises
 
-//Array
+type StringOrNumber = string | number;
 
-let arr = [1, 2, 3, 4, 5];
-
-//Tuples
-
-let arr2: [number, string, boolean] = [1, "Hamid", true];
-
-//Enumerations
-
-enum Color { NAME = 'hamid', AGE = 12 }
-
-//Any
-let a: number;
-a = 12;
-
-//Unknown
-//void
-//null
-//undefined
-//never
-
-
-interface Hamid{
-    name: string;
-    age: number;
-    email: string;
-    password: string;
-    salary?: number; //optional
-}
-
-function hamid(fawaz: Hamid){
-    fawaz.
-}  
-
-interface User {
-    name: string;
-    age: number;
-    email: string;
-    password: string;
-    salary?: number;
-}
-
-interface Admin extends User {
-    admin: boolean;
-}
+let itemID: StringOrNumber;
